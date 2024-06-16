8da479abe6fd5f46f3d05c37e26bab58fc6acb2b
Andy Hudson <andyhudson725@gmail.com>
2024-06-15 20:27:06 -0700

    Use finite state machine for movement state

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    float hSpeed = 0f;
-    float vSpeed = 0f;
-    bool jumpKeyDown = false;
+    private const int ST_GROUND = 0, ST_AIR = 1, ST_GROUND_ATK = 2;
+    private int state = ST_AIR;
 
     public float jumpSpeed = 10.0f;
     public float defaultGroundSpeed = 12.0f;
     public float defaultAirSpeed = 6.0f;
     public float defaultSpeed;
-
-    private float finalMoveSpeed;
-    float fricForce;
-
-    private float distanceToGround;
-    private bool grounded;
+    public int defaultJumps = 1;
+    private int jumps = 0;
 
     public float frictionCoefficient = 0.96f;
     public float grav = 2.25f;
@@ -29,33 +24,36 @@ public class Movement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<BoxCollider2D>();
-        distanceToGround = col.bounds.extents.y;
         rb.gravityScale = grav;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
-    public void TryMove(float hIn, float vIn,float jumpIn)
+    public void TryMove(float hIn, float vIn, float jumpIn)
     {
-        //Set Default Speed
-        defaultSpeed = grounded ? defaultGroundSpeed : defaultAirSpeed;
+        float defaultSpeed = (state == ST_GROUND) ? defaultGroundSpeed : defaultAirSpeed;
 
         //Horizontal Movement Forces
-        hSpeed = hIn * defaultSpeed;
-        fricForce = frictionCoefficient * rb.velocity.x;
+        float hSpeed = hIn * defaultSpeed;
+        float fricForce = frictionCoefficient * rb.velocity.x;
 
         //Vertical Movement Forces
-        vSpeed = (jumpIn * jumpSpeed) * (grounded?1:0);
+        if (jumpIn != 0 && jumps > 0)
+        {
+            float vSpeed = jumpIn * jumpSpeed;
+            rb.AddForce(new Vector2(0, vSpeed), ForceMode2D.Impulse);
+            state = ST_AIR;
+            jumps--;
+        }
 
         //Apply Forces
         rb.AddForce(new Vector2(hSpeed - fricForce, 0));
-        rb.AddForce(new Vector2(0, vSpeed), ForceMode2D.Impulse);
-        
+
         //Stop at Low Speed
         if (Mathf.Abs(rb.velocity.x) < 1)
         {
@@ -64,18 +62,18 @@ public class Movement : MonoBehaviour
     }
 
     //
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (collision.collider.transform.tag == "Ground" && Physics2D.Raycast(rb.position, Vector2.down, distanceToGround + 0.5f, LayerMask.GetMask("Ground")))
+        if (other.transform.tag != "Ground") return;
+
+
+        var detection = Physics2D.Raycast(rb.position, Vector2.down, col.bounds.extents.y + 0.5f, LayerMask.GetMask("Ground"));
+
+        if (detection)
         {
-            grounded = true;
-        } 
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.collider.transform.tag == "Ground")
-        {
-            grounded = false;
+            Debug.Log(detection.distance);
+            state = ST_GROUND;
+            jumps = defaultJumps;
         }
     }
 }
