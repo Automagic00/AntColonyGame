05aff0116263ef93d4bdba2571ef7fb217dd2f96
Andy Hudson <andyhudson725@gmail.com>
2024-08-01 14:55:02 -0700

    Fixed custom editor bugs

--- Assets/Scripts/Util/CustomEditor.cs
+++ Assets/Scripts/Util/CustomEditor.cs
@@ -11,6 +11,7 @@ public class CustomInspector : Editor
         base.OnInspectorGUI();
         if (target is ItemBehavior)
         {
+            // Trigger getter to refresh sprite
             ItemBehavior item = (ItemBehavior)target;
             item.item = item.item;
         }
--- Assets/Scripts/Util/FolderSelectionHelper.cs
+++ Assets/Scripts/Util/FolderSelectionHelper.cs
@@ -11,12 +11,10 @@ using System.IO;
 [InitializeOnLoad]
 public class FolderSelectionHelper
 {
-    //The last object we had selected
-    public static Object lastActiveObject;
-    //Object to select the next frame
-    public static Object objectToSelect;
-    //Whether we have locked the inspector
-    public static bool hasLocked;
+    public static Object lastSelected;
+    public static Object selectThis;
+
+    public static bool inspectorLocked;
 
     static FolderSelectionHelper()
     {
@@ -24,96 +22,61 @@ public class FolderSelectionHelper
         EditorApplication.update += OnUpdate;
 
         //Restore folder lock status
-        hasLocked = EditorPrefs.GetBool("FolderSelectionLocked", false);
+        inspectorLocked = EditorPrefs.GetBool("FolderSelectionLocked", false);
     }
 
     static void OnSelectionChanged()
     {
-        if (lastActiveObject != null && Selection.activeObject != null)
+        if (Selection.activeObject != lastSelected && !IsFolder(Selection.activeObject))
         {
-            //If the selection has actually changed
-            if (Selection.activeObject != lastActiveObject)
-            {
-                //If the new object is a folder, reselect our old object
-                if (IsAssetAFolder(Selection.activeObject))
-                {
-                    //We have to select the object the next frame, otherwise it will not register
-                    objectToSelect = lastActiveObject;
-                }
-                else
-                {
-                    UnLockFolders();
-                    //Update the last object
-                    lastActiveObject = Selection.activeObject;
-                }
-            }
-        }
-        else if (!IsAssetAFolder(Selection.activeObject))
-        {
-            lastActiveObject = Selection.activeObject;
+            lastSelected = Selection.activeObject;
             UnLockFolders();
         }
 
+        if (IsFolder(Selection.activeObject))
+        {
+            selectThis = lastSelected;
+        }
     }
 
     //We have to do selecting in the next editor update because Unity does not allow selecting another object in the same editor update
     static void OnUpdate()
     {
-        //If the editor is locked then we don't care
-        if (objectToSelect != null && !ActiveEditorTracker.sharedTracker.isLocked)
-        {
-            //Select the new object
-            Selection.activeObject = objectToSelect;
+        if (ActiveEditorTracker.sharedTracker.isLocked) return;
+        if (selectThis == null) return;
 
-            LockFolders();
+        Selection.activeObject = selectThis;
 
-            lastActiveObject = objectToSelect;
-            objectToSelect = null;
-        }
-        else
-        {
-            objectToSelect = null;
-        }
+        LockFolders();
+
+        lastSelected = selectThis;
+        selectThis = null;
     }
 
-    static void LockFolders()
+    private static void LockFolders()
     {
         ActiveEditorTracker.sharedTracker.isLocked = true;
-        hasLocked = true;
+        inspectorLocked = true;
         //We store the state so that if we compile or leave the editor while the folders are locked then the state is kept
         EditorPrefs.SetBool("FolderSelectionLocked", true);
     }
 
-    static void UnLockFolders()
+    private static void UnLockFolders()
     {
-        //Only unlock inspector if we are the one who locked it
-        if (hasLocked)
-        {
-            ActiveEditorTracker.sharedTracker.isLocked = false;
-            hasLocked = false;
-            EditorPrefs.SetBool("FolderSelectionLocked", false);
-        }
+        if (!inspectorLocked) return;
+
+        ActiveEditorTracker.sharedTracker.isLocked = false;
+        inspectorLocked = false;
+        EditorPrefs.SetBool("FolderSelectionLocked", false);
     }
 
-    private static bool IsAssetAFolder(Object obj)
+    private static bool IsFolder(Object obj)
     {
-        string path = "";
-
         if (obj == null)
-        {
             return false;
-        }
 
-        //Get the path to the asset
-        path = AssetDatabase.GetAssetPath(obj.GetInstanceID());
-
-        //If the asset is a directory (i.e a folder)
-        if (path.Length > 0 && Directory.Exists(path))
-        {
-            return true;
-        }
-
-        return false;
+        string path = AssetDatabase.GetAssetPath(obj.GetInstanceID());
+        return path.Length > 0 && Directory.Exists(path);
     }
 
 }
